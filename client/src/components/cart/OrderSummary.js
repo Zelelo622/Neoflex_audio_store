da2a7d654f3e561a9ee908186baebb2f15c09a27
Egor Kirin <egorcom27@gmail.com>
2023-04-30 01:47:18 +0300

    верстка блока оформления заказа

--- client/src/components/cart/OrderSummary.js
+++ client/src/components/cart/OrderSummary.js
@@ -5,16 +5,12 @@ const OrderSummary = ({ totalQuantity, totalPrice }) => {
     <div className="order-summary">
       <div className="order-summary__inner">
         <div className="order-summary__item">
-          <div className="order-summary__title">Количество товаров:</div>
-          <div className="order-summary__value">{totalQuantity}</div>
-        </div>
-        <div className="order-summary__item">
           <div className="order-summary__title">Итого:</div>
-          <div className="order-summary__value">{totalPrice.toFixed(2)} ₽</div>
+          <div className="order-summary__value">₽ {totalPrice.toFixed(2)}</div>
         </div>
         <div className="order-summary__btn-cont">
-          <button variant="primary" className="order-summary__button button">
-            Продолжить оформление
+          <button variant="primary" className="order-summary__button">
+            Перейти к оформлению
           </button>
         </div>
       </div>
